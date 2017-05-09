0599056eb03a53e4af78aee3d5856e3c238c82ea
atulsingla82 <Lucky@Atuls-MBP.fios-router.home>
2017-05-08 23:29:03 -0400

    updates

---
+++ app/data/friends.js
@@ -0,0 +1,41 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+
+var friendArray = [
+{
+        name: "Drax - The destroyer",
+        photo: "https://vignette3.wikia.nocookie.net/marvelcrossroads/images/8/8c/Drax_the_Destroyer_%28Earth-014%29.png/revision/latest?cb=20151115172248",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    }, {
+        name: "Star Lord",
+        photo: "https://www.sideshowtoy.com/wp-content/uploads/2014/07/902219-product-silo.png",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+
+}
+
+]
+
+
+module.exports = friendArray;
